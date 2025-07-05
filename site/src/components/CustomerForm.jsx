2e879ad1aaedc4730e9f74e1eea5b5c6e36e43d6
matheusferreira <mattheusfferreira@gmail.com>
2025-07-04 23:44:01 -0300

    feat(customer-form):Update custumer form into a component

---
+++ site/src/components/CustomerForm.jsx
@@ -0,0 +1,46 @@
+import { useRef } from 'react'
+import { useTranslation } from 'react-i18next';
+
+import api from '../services/api'
+
+export default function CustomerForm() {
+    const { t, i18n } = useTranslation();
+
+    const inputName = useRef()
+    const inputTaxCode = useRef()
+    const inputEmail = useRef()
+    const inputPhone = useRef()
+
+    const handleLanguageChange = (lang) => {
+        i18n.changeLanguage(lang);
+    };
+
+
+    async function createCustomer() {
+        const customerObj = {
+            name: inputName.current.value,
+            taxCode: inputTaxCode.current.value,
+            email: inputEmail.current.value,
+            telefone: inputPhone.current.value
+        }
+
+        await api.post('/Customer', customerObj)
+    }
+    return (
+        <div className='container'>
+            <div>
+                <button onClick={() => handleLanguageChange('pt')}>Português</button>
+                <button onClick={() => handleLanguageChange('en')}>English</button>
+            </div>
+            <form>
+                <h1>{t('customerForm.title')}</h1>
+                <input placeholder={t('customerForm.name')} name='name' type='text' ref={inputName}></input>
+                <input placeholder={t('customerForm.taxCode')} name='taxCode' type='text' ref={inputTaxCode}></input>
+                <input placeholder={t('customerForm.email')} name='email' type='email' ref={inputEmail}></input>
+                <input placeholder={t('customerForm.phone')} name='phone' type='text' ref={inputPhone}></input>
+
+                <button type='button' onClick={createCustomer}>{t('customerForm.saveButton')}</button>
+            </form>
+        </div>
+    )
+};
